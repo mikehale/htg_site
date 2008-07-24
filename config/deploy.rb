655450ea54307a69ab4126e423146f53df49a231
Michael Hale <mikehale@gmail.com>
2008-07-24 16:59:53 -0400

    checkin schema.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,10 @@
 set :application, "site"
 set :domain, "slice.halethegeek.com"
-set :repository, "git@github.com:mikehale/ignite_same.git"
+set :repository, "git@github.com:mikehale/htg_site.git"
+set :deploy_to, "/var/www/apps/#{application}"
 
 set :user, "deploy"
+set :use_sudo, false
 set :rails_env, "production" 
 set :scm, "git"
 set :deploy_via, :remote_cache
@@ -13,3 +15,17 @@ default_run_options[:pty] = true
 role :web, domain
 role :app, domain
 role :db,  domain, :primary => true
+
+# after "deploy:update", "gems:install"
+# 
+# namespace :gems do
+#   task :install do
+#     sudo "sh -c'cd #{current_path} && rake gems:install'"
+#   end  
+# end
+
+after "deploy:symlink", "symlink_database_yml"
+
+task :symlink_database_yml do
+  run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"  
+end
